4d08ce79d2cae061f0075898728a3ac641418108
Ksingh210 <48933892+Ksingh210@users.noreply.github.com>
2019-10-12 19:57:02 -0700

    inital comment

--- scrapperdb.py
+++ scrapperdb.py
@@ -4,7 +4,7 @@ from mysql.connector import Error
 try:
     connection = mysql.connector.connect(host='localhost',
                                          user='root',
-                                         password='multani7',
+                                         password='django1234',
                                          database='amazonwebscrapperdb')
 except Error as e:
     print("Error while connecting to MySQL", e)
